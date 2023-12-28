a8ce8f4d95850c395827a8c9a84c89462ee216c2
skif- <44skif99@gmail.com>
2023-12-28 18:34:42 +0300

    final_project2.0

--- src/main/java/by/itclass/model/dao/UserDao.java
+++ src/main/java/by/itclass/model/dao/UserDao.java
@@ -4,12 +4,25 @@ import by.itclass.model.db.ConnectionManager;
 import by.itclass.model.entities.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Objects;
 
 import static by.itclass.constants.DbConstants.*;
 
 public class UserDao {
+    private static UserDao dao;
+
+    public UserDao() {
+        ConnectionManager.init();
+    }
+
+    public static UserDao getInstance() {
+        if (Objects.isNull(dao)) {
+            dao = new UserDao();
+        }
+        return dao;
+    }
 
     public User getUser(String login, String password) {
         try (var cn = ConnectionManager.getConnection();
@@ -31,10 +44,11 @@ public class UserDao {
 
     public boolean addUser(User user, String password) {
         try (var cn = ConnectionManager.getConnection();
-             var psInsert = cn.prepareStatement(INSERT_USER);
+             var ps = cn.prepareStatement(INSERT_USER);
              var psSelect = cn.prepareStatement(SELECT_USERID_BY_LOGIN)) {
             psSelect.setString(1, user.getLogin());
-            if (isAccessible() {
+
+            if (isAccessible(psSelect)) {
                 ps.setString(1, user.getName());
                 ps.setString(2, user.getEmail());
                 ps.setString(3, user.getLogin());
@@ -47,13 +61,7 @@ public class UserDao {
         return false;
     }
 
-    private boolean isAccessible(String login, Connection cn) throws SQLException {
-        try ) {
-            ps.setString(1, login);
+    private boolean isAccessible(PreparedStatement ps) throws SQLException {
             return !ps.executeQuery().next();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 }
